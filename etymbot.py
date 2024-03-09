e5ffcfe61b4cb8c73314970d446f6319a5a789fa
dopc <aliacar23@gmail.com>
2024-03-09 15:52:57 +0300

    update bot

--- etymbot.py
+++ etymbot.py
@@ -4,29 +4,38 @@ from bs4 import BeautifulSoup
 import requests
 import logging
 from telegram import InlineQueryResultArticle, InputTextMessageContent
+import os
 
-updater = Updater(token='TOKEN')
+logger = logging.getLogger(__name__)
+
+updater = Updater(token=os.getenv("BOT_TOKEN"))
 dispatcher = updater.dispatcher
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 SEARCH_URL = 'https://www.etymonline.com/search?q='
 
 
 def inline_etym(update, context):
     query = update.inline_query.query
-    results = list()
-    text = first_result_return(query)
-    results.append(InlineQueryResultArticle(
-        id=query,
-        title=query,
-        input_message_content=InputTextMessageContent(query + ': \n' + text)))
+    try:
+        logger.info(f"Query: {query}")
+        results = list()
+        text = first_result_return(query)
+        content = f"{query}:\n{text}"
+        results.append(InlineQueryResultArticle(
+            id=query,
+            title=query,
+            input_message_content=InputTextMessageContent(content))
+        )
+        context.bot.answer_inline_query(update.inline_query.id, results)
+    except AttributeError:
+        pass
 
-    context.bot.answer_inline_query(update.inline_query.id, results)
 
-
-inline_etym_handler = InlineQueryHandler(inline_etym)
-dispatcher.add_handler(inline_etym_handler)
-updater.start_polling()
+if __name__ == '__main__':
+    inline_etym_handler = InlineQueryHandler(inline_etym)
+    dispatcher.add_handler(inline_etym_handler)
+    updater.start_polling()
 
 
 '''
